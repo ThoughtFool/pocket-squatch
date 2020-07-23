5c66408195c2c2349b962debaa1b50f0cd84efef
ThoughtFool <thotfool@gmail.com>
2020-07-23 01:27:51 -0400

    still more tweaking of sprite collision-detection

--- public/logic/class/sprite-physics-class.js
+++ public/logic/class/sprite-physics-class.js
@@ -268,13 +268,13 @@ class Sprite_Physics {
 
                 } else if (this.left < elem_coords.right && this.left + (this.width * .75) > elem_coords.right &&
                     this.top < elem_coords.bottom && this.top + (this.height * .75) > elem_coords.bottom) { // top-left side collison
-                    // alert(`top-left side collision: this.left: ${this.left}, this.right: ${this.left + this.width}, elem_coords.right: ${elem_coords.right}`);
+                    console.info(`top-left side collision: this.left: ${this.left}, this.right: ${this.left + this.width}, elem_coords.right: ${elem_coords.right}`);
 
                     return this.left = elem_coords.right + 1; // lands next to obstacle (right-side)
 
                 } else if (this.left + this.width > elem_coords.left && this.left + (this.width * .75) < elem_coords.left &&
                     this.top < elem_coords.bottom && this.top + (this.height * .75) > elem_coords.bottom) { // top-right side collison
-                    // alert(`top-right side collision:`);
+                    console.info(`top-right side collision:`);
 
                     return this.left = elem_coords.left - this.width - 1; // lands next to obstacle (left-side)
 
